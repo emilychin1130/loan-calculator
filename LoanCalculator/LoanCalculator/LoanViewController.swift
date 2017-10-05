73c9b925d16f0c2ef69a17f818f8c19e719fa00f
emilychin1130 <emilychin1130@gmail.com>
2017-10-04 19:12:45 -0700

    added finishing touches

--- LoanCalculator/LoanCalculator/LoanViewController.swift
+++ LoanCalculator/LoanCalculator/LoanViewController.swift
@@ -10,11 +10,35 @@ import Foundation
 import UIKit
 
 class LoanViewController: UIViewController {
+    override func viewDidLoad() {
+        monthlyPaymentTextField.keyboardType = UIKeyboardType.numberPad
+        termTextField.keyboardType = UIKeyboardType.numberPad
+        rateField.keyboardType = UIKeyboardType.numberPad
+        
+        //init toolbar
+        let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0,  width: self.view.frame.size.width, height: 30))
+        //create left side empty space so that done button set on right side
+        let flexSpace = UIBarButtonItem(barButtonSystemItem:    .flexibleSpace, target: nil, action: nil)
+        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: Selector("doneButtonAction"))
+        toolbar.setItems([flexSpace, doneBtn], animated: false)
+        toolbar.sizeToFit()
+        //setting toolbar as inputAccessoryView
+        monthlyPaymentTextField.inputAccessoryView = toolbar
+        termTextField.inputAccessoryView = toolbar
+        rateField.inputAccessoryView = toolbar
+    }
+    
+    func doneButtonAction() {
+        self.view.endEditing(true)
+    }
+    
     @IBOutlet weak var monthlyPaymentTextField: UITextField!
     @IBOutlet weak var termTextField: UITextField!
     @IBOutlet weak var rateField: UITextField!
     
-    @IBOutlet weak var monthlyPaymentLabel: UILabel!
+    @IBOutlet weak var loanAmountLabel: UILabel!
+    @IBOutlet weak var totalInterestLabel: UILabel!
+    @IBOutlet weak var totalPaymentLabel: UILabel!
     
     @IBAction func calculateButton(_ sender: Any) {
         if let monthlyPayment = Double(monthlyPaymentTextField.text!) {
@@ -22,18 +46,26 @@ class LoanViewController: UIViewController {
                 if let rate = Double(rateField.text!) {
                     let numerator = monthlyPayment * (1.0 - (pow((1.0 + rate/1200),(0.0 - term*12))))
                     let denominator = rate/1200
-                    let answer = numerator / denominator
-                    // monthlyPaymentLabel.text = "\(answer)"
-                    monthlyPaymentLabel.text = String(format: "%.2f", answer)
-                    print("\(answer)")
+                    let loanAmount = numerator / denominator
+                    let totalPayment = monthlyPayment * term * 12
+                    let totalInterest = totalPayment - loanAmount
+                    loanAmountLabel.text = "$\(String(format: "%.2f", loanAmount))"
+                    totalPaymentLabel.text = "$\(String(format: "%.2f", totalPayment))"
+                    totalInterestLabel.text = "$\(String(format: "%.2f", totalInterest))"
                 } else {
-                    monthlyPaymentLabel.text = ""
+                    loanAmountLabel.text = ""
+                    totalPaymentLabel.text = ""
+                    totalInterestLabel.text = ""
                 }
             } else {
-                monthlyPaymentLabel.text = ""
+                loanAmountLabel.text = ""
+                totalPaymentLabel.text = ""
+                totalInterestLabel.text = ""
             }
         } else {
-            monthlyPaymentLabel.text = ""
+            loanAmountLabel.text = ""
+            totalPaymentLabel.text = ""
+            totalInterestLabel.text = ""
         }
     }
     
--- LoanCalculator/LoanCalculator/MonthViewController.swift
+++ LoanCalculator/LoanCalculator/MonthViewController.swift
@@ -10,11 +10,36 @@ import Foundation
 import UIKit
 
 class MonthViewController: UIViewController {
+    override func viewDidLoad() {
+        loanAmountTextField.keyboardType = UIKeyboardType.decimalPad
+        termTextField.keyboardType = UIKeyboardType.numberPad
+        rateField.keyboardType = UIKeyboardType.numberPad
+        
+        //init toolbar
+        let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0,  width: self.view.frame.size.width, height: 30))
+        //create left side empty space so that done button set on right side
+        let flexSpace = UIBarButtonItem(barButtonSystemItem:    .flexibleSpace, target: nil, action: nil)
+        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: Selector("doneButtonAction"))
+        toolbar.setItems([flexSpace, doneBtn], animated: false)
+        toolbar.sizeToFit()
+        //setting toolbar as inputAccessoryView
+        loanAmountTextField.inputAccessoryView = toolbar
+        termTextField.inputAccessoryView = toolbar
+        rateField.inputAccessoryView = toolbar
+    }
+    
+    func doneButtonAction() {
+        self.view.endEditing(true)
+    }
+    
     @IBOutlet weak var loanAmountTextField: UITextField!
     @IBOutlet weak var termTextField: UITextField!
     @IBOutlet weak var rateField: UITextField!
         
     @IBOutlet weak var monthlyPaymentLabel: UILabel!
+    @IBOutlet weak var totalInterestLabel: UILabel!
+
+    @IBOutlet weak var totalPaymentLabel: UILabel!
     
     @IBAction func calculateButton(_ sender: Any) {
         if let loanAmount = Double(loanAmountTextField.text!) {
@@ -22,18 +47,26 @@ class MonthViewController: UIViewController {
                 if let rate = Double(rateField.text!) {
                     let numerator = ((rate/1200.0) * loanAmount)
                     let denominator = 1.0 - (pow((1.0 + rate/1200),(0.0 - term*12)))
-                    let answer = numerator / denominator
-                   // monthlyPaymentLabel.text = "\(answer)"
-                    monthlyPaymentLabel.text = String(format: "%.2f", answer)
-                    print("\(answer)")
+                    let monthlyPayment = numerator / denominator
+                    let totalPayment = monthlyPayment * term * 12
+                    let totalInterest = totalPayment - loanAmount
+                    monthlyPaymentLabel.text = "$\(String(format: "%.2f", monthlyPayment))"
+                    totalInterestLabel.text = "$\(String(format: "%.2f", totalInterest))"
+                    totalPaymentLabel.text = "$\(String(format: "%.2f", totalPayment))"
                 } else {
                     monthlyPaymentLabel.text = ""
+                    totalInterestLabel.text = ""
+                    totalPaymentLabel.text = ""
                 }
             } else {
                 monthlyPaymentLabel.text = ""
+                totalInterestLabel.text = ""
+                totalPaymentLabel.text = ""
             }
         } else {
             monthlyPaymentLabel.text = ""
+            totalInterestLabel.text = ""
+            totalPaymentLabel.text = ""
         }
     }
 
